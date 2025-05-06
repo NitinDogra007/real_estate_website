f05c85c015963750f4b8d636331e6dd8f07f4ee2
Nitin Dogra <90884243+NitinDogra007@users.noreply.github.com>
2025-05-06 23:53:10 +1000

    add contact, testimonial and project page

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
-import About from './components/About';
-import Header from './components/Header';
-import Projects from './components/Projects';
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Header from "./components/Header";
+import Projects from "./components/Projects";
+import Testimonials from "./components/Testimonials";
 
 function App() {
-	return (
-		<div className="w-full overflow-hidden">
-			<Header />
-			<About />
-			<Projects />
-		</div>
-	);
+  return (
+    <div className="w-full overflow-hidden">
+      <Header />
+      <About />
+      <Projects />
+      <Testimonials />
+      <Contact />
+    </div>
+  );
 }
 
 export default App;
